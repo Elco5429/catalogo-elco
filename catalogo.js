045778c8e64e7454d17bb88f66c8e67134f1d879
Elco5429 <elcomarketing5@gmail.com>
2026-01-06 12:34:45 -0600

    Update catalogo.js

--- catalogo.js
+++ catalogo.js
@@ -30,5 +30,36 @@ function cerrarDetalles() {
     document.querySelectorAll('.detalle-servicio').forEach(sec => {
         sec.style.display = 'none';
     });
+}function abrirCotizacion() {
+    document.getElementById("modalCotiza").style.display = "flex";
 }
+
+function cerrarCotizacion() {
+    document.getElementById("modalCotiza").style.display = "none";
+}
+
+function enviarWhatsApp() {
+    const nombre = document.getElementById("nombre").value;
+    const servicios = [...document.getElementById("servicio").selectedOptions]
+        .map(op => op.value)
+        .join(", ");
+    const mensaje = document.getElementById("mensaje").value;
+
+    if (!nombre || !servicios) {
+        alert("Completa los campos obligatorios");
+        return;
+    }
+
+    const texto = `Hola, soy ${nombre}.
+Me interesa cotizar los siguientes servicios:
+${servicios}
+
+Proyecto:
+${mensaje}`;
+
+    const url = `https://wa.me/527224556699?text=${encodeURIComponent(texto)}`;
+    window.open(url, "_blank");
+}
+
+
 
